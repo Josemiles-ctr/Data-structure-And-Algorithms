13a3fb2211a56531947b0b42ca0261c8852e6224
OTAI JOSEPH <josephotai37@gmail.com>
2025-10-23 16:44:47 +0300

    Implementing the different data types in C

--- Lecture1/C/builtIn.c
+++ Lecture1/C/builtIn.c
@@ -1,4 +1,48 @@
-# include <stdio.h>
+#include <stdio.h>
+#include <stdbool.h>
 int main() {
+    /*Numeric types*/
+    int x;
+    double y;
+    float z;
+
+    /*Charater types*/
+    char k;
+
+    /*Boolean types*/
+    bool b =1;
+    printf("%d\n", b);
+    /*Voids*/
+    void *p;
+
+    /*Derived Types*/
+    int students[10];
+    char name[5];
+    int *ptr;
+    int add(int x, int y);
+    struct student
+    {
+        int age;
+        char name[20];
+    };
+    union lecture
+    {
+        int age;
+        char courseUnit[30];
+    };
+    enum week{Mon, Tue, Web, Thur, Fri, Sat, Sun};
+
+
+
+    /*Modifiers*/
+    short x1;
+    long x2;
+    long long x3;
+    long double f;
+    signed int s1;
+    signed char g1;
+    unsigned int s2;
+    unsigned char g2;
+
     return 0;
 }
