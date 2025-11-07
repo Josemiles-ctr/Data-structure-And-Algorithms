bfbeaccd970bdb5d7c9b2f62aea76f893e4b24dd
Dorie-xa <agabadoreck1@gmail.com>
2025-11-07 10:42:36 +0300

    array deletion

--- DSAinC/Arraydeletion.c
+++ DSAinC/Arraydeletion.c
@@ -7,7 +7,7 @@ int main(){
     int numbers[7] = {23,2,56,34,13};  // Unfilled array
     int size = 7; // The size of the array
     int n = 5; //Number of elements in the array
-    int value = 56; // Value intended for deletion
+    int value = 5; // Value intended for deletion
     int index = search_array(numbers,value,n); // The index of the element intended for deletion
 
      printf("Array before deletion \t");
@@ -39,7 +39,7 @@ int main(){
 
 int search_array(int arr[], int intendedValue, int arrElements){
     //Attempt to search through an array for a value and return its index
-    int index;  // Holder of the intended value's index
+    int index=-1;  // Holder of the intended value's index
 
     for(int i = 0; i < arrElements; i++){
         if(arr[i] == intendedValue){   //Acquire the index of the intended element
